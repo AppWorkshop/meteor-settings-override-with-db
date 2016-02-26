d3e8df6c027bae4086a57ab7262f31c4d9711fa8
Mike Cunneen <mike@cunneen.biz>
2016-02-26 16:11:52 +0800

    bump version to 1.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'appworkshop:settings-override-with-db',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'Allow override of Meteor.settings with DB.',
   // URL to the Git repository containing the source code for this package.
